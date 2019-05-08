f0b0d6d5750a1b0a3e04ea74a9892d6575bc89c6
Salikh Zakirov <salikh@google.com>
2019-05-08 11:46:03 +0900

    (uploadsserver) serve reports with autoreload and backoff

--- go/uploadserver/uploadserver.go
+++ go/uploadserver/uploadserver.go
@@ -6,8 +6,11 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path"
 	"path/filepath"
 	"reflect"
+	"time"
 
 	"github.com/golang/glog"
 	"github.com/google/prog-edu-assistant/queue"
@@ -29,20 +32,23 @@ type Options struct {
 }
 
 type Server struct {
-	opts Options
-	mux  *http.ServeMux
+	opts            Options
+	mux             *http.ServeMux
+	reportTimestamp map[string]time.Time
 }
 
 func New(opts Options) *Server {
 	mux := http.NewServeMux()
 	s := &Server{
-		opts: opts,
-		mux:  mux,
+		opts:            opts,
+		mux:             mux,
+		reportTimestamp: make(map[string]time.Time),
 	}
 	mux.Handle("/", handleError(s.uploadForm))
 	mux.Handle("/upload", handleError(s.handleUpload))
 	mux.Handle("/uploads/", http.StripPrefix("/uploads",
 		http.FileServer(http.Dir(s.opts.UploadDir))))
+	mux.Handle("/report/", handleError(s.handleReport))
 	return s
 }
 
@@ -64,7 +70,83 @@ func handleError(fn func(http.ResponseWriter, *http.Request) error) http.Handler
 	})
 }
 
-type httpHandleFuncWithError func(http.ResponseWriter, *http.Request) error
+func (s *Server) handleReport(w http.ResponseWriter, req *http.Request) error {
+	if s.opts.DisableCORS {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+	}
+	basename := path.Base(req.URL.Path)
+	filename := filepath.Join(s.opts.UploadDir, basename+".txt")
+	glog.V(5).Infof("checking %q for existence", filename)
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		// Serve a placeholder autoreload page.
+		reloadMs := int64(500)
+		ts := s.reportTimestamp[basename]
+		if ts.IsZero() {
+			// Store the first request time
+			s.reportTimestamp[basename] = time.Now()
+			// TODO(salikh): Eventually clean up old entries from reportTimestamp map.
+		} else {
+			// Back off automatically.
+			reloadMs = time.Since(ts).Nanoseconds() / 1000000
+		}
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		// TODO(salikh): Make timeout configurable.
+		if reloadMs > 10000 {
+			// Reset for retry.
+			reloadMs = 500
+			s.reportTimestamp[basename] = time.Now()
+		}
+		if reloadMs > 5000 {
+			fmt.Fprintf(w, `<title>Something weng wrong</title>
+<h2>Error</h2>
+Something went wrong, please retry your upload.
+`)
+			return nil
+		}
+		fmt.Fprintf(w, `<title>Please wait</title>
+<script>
+function refresh(t) {
+	setTimeout("location.reload(true)", t)
+}
+</script>
+<body onload="refresh(%d)">
+<h2>Waiting for %d seconds, report is being generated now</h2>
+</body>`, reloadMs, (reloadMs+999)/1000)
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	data := make(map[string]interface{})
+	err = json.Unmarshal(b, &data)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<title>Report for %s</title>`, basename)
+	if v, ok := data["reports"]; ok {
+		reports, ok := v.(map[string]interface{})
+		if !ok {
+			return fmt.Errorf("expected reports to be map[string]interface{}, got %s", reflect.TypeOf(v))
+		}
+		// Just concatenate all reports.
+		for exercise_id, report := range reports {
+			fmt.Fprintf(w, "<h2>%s</h2>", exercise_id)
+			html, ok := report.(string)
+			if !ok {
+				return fmt.Errorf("expected report to be a string, got %s", reflect.TypeOf(report))
+			}
+			fmt.Fprint(w, html)
+		}
+	}
+	return nil
+}
 
 const maxUploadSize = 1048576
 
@@ -128,11 +210,10 @@ func (s *Server) handleUpload(w http.ResponseWriter, req *http.Request) error {
 	if err != nil {
 		return err
 	}
-	reportfilename := submissionID + ".txt"
 	w.Header().Set("Content-Type", "text/plain")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
-	fmt.Fprintf(w, "/uploads/"+ reportfilename)
+	fmt.Fprintf(w, "/report/"+submissionID)
 	return nil
 }
 
@@ -161,7 +242,7 @@ func (s *Server) ListenForReports(ch <-chan []byte) {
 			continue
 		}
 		// TODO(salikh): Write a pretty report instead.
-		filename := filepath.Join(s.opts.UploadDir, submissionID + ".txt")
+		filename := filepath.Join(s.opts.UploadDir, submissionID+".txt")
 		err = ioutil.WriteFile(filename, b, 0775)
 		if err != nil {
 			glog.Errorf("Error writing to %q: %s", filename, err)
