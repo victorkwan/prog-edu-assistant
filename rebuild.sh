ce2571df2a1fa8f727f2f9c62e6470cf323cbed9
Salikh Zakirov <salikh@google.com>
2019-05-28 23:04:13 +0900

    drop oop-1 from rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -10,9 +10,7 @@ cd go
 # Generate student notebook
 go run cmd/assign/assign.go --command student --input=../exercises/helloworld-en-master.ipynb --output=../tmp-student/helloworld-en-student.ipynb
 go run cmd/assign/assign.go --command student --input=../exercises/oop-en-master.ipynb --output=../tmp-student/oop-en-student.ipynb
-go run cmd/assign/assign.go --command student --input=../exercises/oop-1-en-master.ipynb --output=../tmp-student/oop-1-en-student.ipynb
 
 # Generate the autograder script directories
 go run cmd/assign/assign.go --command=autograder --input=../exercises/helloworld-en-master.ipynb --output=../tmp-autograder
 go run cmd/assign/assign.go --command=autograder --input=../exercises/oop-en-master.ipynb --output=../tmp-autograder
-go run cmd/assign/assign.go --command=autograder --input=../exercises/oop-1-en-master.ipynb --output=../tmp-autograder
