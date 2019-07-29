f02dba1d78b9e59d27c11897c20d0862e22df72b
Salikh Zakirov <salikh@google.com>
2019-07-30 04:31:10 +0900

    deploy-docker.sh: kill previous service on deploy

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -16,6 +16,6 @@ set -e
   deploy/{certs,docker-compose.yml,secret.env,service-account.json} \
   $GCE_HOST:
 
-@execute ssh $GCE_HOST "mkdir -p logs && cat service-account.json | docker login -u _json_key --password-stdin https://asia.gcr.io && docker pull asia.gcr.io/prog-edu-assistant/worker && docker pull asia.gcr.io/prog-edu-assistant/server && docker run -d --rm -v /var/run/docker.sock:/var/run/docker.sock -v \$PWD:\$PWD -w=\$PWD --entrypoint=sh docker/compose:1.24.0 -c 'cat service-account.json | docker login -u _json_key --password-stdin https://asia.gcr.io && docker-compose up --scale worker=2'"
+@execute ssh $GCE_HOST "mkdir -p logs && docker ps -q | xargs -n1 docker kill; cat service-account.json | docker login -u _json_key --password-stdin https://asia.gcr.io && docker pull asia.gcr.io/prog-edu-assistant/worker && docker pull asia.gcr.io/prog-edu-assistant/server && docker run -d --rm -v /var/run/docker.sock:/var/run/docker.sock -v \$PWD:\$PWD -w=\$PWD --entrypoint=sh docker/compose:1.24.0 -c 'cat service-account.json | docker login -u _json_key --password-stdin https://asia.gcr.io && docker-compose up --scale worker=2'"
 
 echo OK
